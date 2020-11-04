29d97bb950edead4b3c2dbc559beeaae0531ab2f
Anna Garczarek <ania.garczarek@gmail.com>
2020-11-04 17:41:42 +0100

    Modify hero files

--- src/components/layout/Hero/Hero.js
+++ src/components/layout/Hero/Hero.js
@@ -5,13 +5,10 @@ import styles from './Hero.module.scss';
 const Hero = () => (
   <div className={styles.root}>
     <div className='container '>
-      <div className='row align-items-center justify-content-end'>
-        <div className={`col ${styles.titles}`}>
+      <div className='row align-items-end justify-content-center'>
+        <div className={'col pt-5 ' + styles.titles}>
           <div className={styles.subtitle}>Go green.</div>
-          <div className={styles.title}>The world of plants</div>
-        </div>
-        <div className={styles.description}>
-          Buy and discover everything you need to know about your plants, treat them with kindness and they will take care of you.
+          <div className={styles.title}>The world of wonderful plants</div>
         </div>
       </div>
     </div>
