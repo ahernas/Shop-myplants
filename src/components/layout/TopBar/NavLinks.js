f11820339492d2b01c006ab38200c75ff9c3ddcc
Anna Garczarek <ania.garczarek@gmail.com>
2022-02-14 18:32:41 +0100

    Fix RWD in SpecialPriceBox

--- src/components/layout/TopBar/NavLinks.js
+++ src/components/layout/TopBar/NavLinks.js
@@ -5,29 +5,54 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const NavLinks = (props) => {
+  const animateFrom = {opacity: 0, y: -40};
+  const animateTo = {opacity: 1, y: 0};
+
+
   return (
     <ul>
-      <li onClick={() => props.isMobile && props.closeMobileMenu()}>
+      <li
+        initial={animateFrom}
+        animate={animateTo}
+        onClick={() => props.isMobile && props.closeMobileMenu()}>
         <a href="/">Home</a>
       </li>
-      <li onClick={() => props.isMobile && props.closeMobileMenu()}>
+      <li
+        initial={animateFrom}
+        animate={animateTo}
+        onClick={() => props.isMobile && props.closeMobileMenu()}>
         <a href="/products">Products</a>
       </li>
-      <li onClick={() => props.isMobile && props.closeMobileMenu()}>
+      <li
+        initial={animateFrom}
+        animate={animateTo}
+        onClick={() => props.isMobile && props.closeMobileMenu()}>
         <a href="/plantFinder">Plant finder</a>
       </li>
-      <li onClick={() => props.isMobile && props.closeMobileMenu()}>
+      <li
+        initial={animateFrom}
+        animate={animateTo}
+        onClick={() => props.isMobile && props.closeMobileMenu()}>
         <a href="/aboutUs">About us</a>
       </li>
-      <li onClick={() => props.isMobile && props.closeMobileMenu()}>
+      <li
+        initial={animateFrom}
+        animate={animateTo}
+        onClick={() => props.isMobile && props.closeMobileMenu()}>
         <a href="/contacts">Contact</a>
       </li>
-      <li onClick={() => props.isMobile && props.closeMobileMenu()}>
+      <li
+        initial={animateFrom}
+        animate={animateTo}
+        onClick={() => props.isMobile && props.closeMobileMenu()}>
         <a href="/user">
           <FontAwesomeIcon className={`icons ${styles.icon}`} icon={faUser}/>
         </a>
       </li>
-      <li onClick={() => props.isMobile && props.closeMobileMenu()}>
+      <li
+        initial={animateFrom}
+        animate={animateTo}
+        onClick={() => props.isMobile && props.closeMobileMenu()}>
         <a href="/cart">
           <FontAwesomeIcon className={styles.icon} icon={faShoppingCart}/>
         </a>
